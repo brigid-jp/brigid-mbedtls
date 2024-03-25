453e0b31da0ca26d5151af9840888ce1e03ed9c0
dev <dev@brigid.jp>
2024-03-26 00:27:05 +0900

    テストを修正。

--- test/test_runtime_error_policy.lua
+++ test/test_runtime_error_policy.lua
@@ -1,5 +1,7 @@
 local mbedtls = require "brigid.mbedtls"
 
+local debug = (tonumber(os.getenv "BRIGID_DEBUG") or 0) > 0
+
 assert(mbedtls.get_runtime_error_policy() == "fail")
 
 local result, message = mbedtls.pk():setup(mbedtls.pk.NONE)
