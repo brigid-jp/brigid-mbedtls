78b2cc5ab81ab8fd77e58f21defd14a25c258063
dev <dev@brigid.jp>
2024-03-26 18:20:13 +0900

    テストを修正。

--- test/test_ctr_drbg.lua
+++ test/test_ctr_drbg.lua
@@ -1,8 +1,22 @@
 local mbedtls = require "brigid.mbedtls"
+mbedtls.set_runtime_error_policy "error"
+local test = require "test"
+
+local n = 16
+
+local entropy = mbedtls.entropy()
+local data = entropy:func(n)
+assert(#data == n)
+if test.debug then
+  print(("%02X"):rep(n):format(string.byte(data, 1, n)))
+end
 
 local ctr_drbg = mbedtls.ctr_drbg()
-ctr_drbg:seed(mbedtls.entropy())
+ctr_drbg:seed(entropy)
 collectgarbage()
 collectgarbage()
-local data = ctr_drbg:random(8)
-assert(#data == 8)
+local data = ctr_drbg:random(n)
+assert(#data == n)
+if test.debug then
+  print(("%02X"):rep(n):format(string.byte(data, 1, n)))
+end
--- test/test_entropy.lua
+++
@@ -1,5 +0,0 @@
-local mbedtls = require "brigid.mbedtls"
-
-local entropy = mbedtls.entropy()
-local data = entropy:func(8)
-assert(#data == 8)
