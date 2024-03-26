6961f901939152892ab1cb988561824f2aff699a
dev <dev@brigid.jp>
2024-03-27 00:05:37 +0900

    テストを追加。

--- test/test.lua
+++ test/test.lua
@@ -21,3 +21,22 @@ assert(mbedtls.get_runtime_error_policy() == "fail")
 test.assume_fail(setup_pk_none)
 
 assert(debug.getregistry()["brigid.mbedtls.runtime_error_policy"] == "fail")
+
+local version = mbedtls.get_version()
+if test.debug then
+  print("version: "..version)
+end
+assert(version:match "^%d+%.%d+$")
+
+-- 0x03 05 02 00
+local n = mbedtls.version.get_number()
+local s = mbedtls.version.get_string()
+local f = mbedtls.version.get_string_full()
+if test.debug then
+  print(("number: 0x%08X"):format(n))
+  print("string: "..s)
+  print("string_full: "..f)
+end
+local x, y, z = assert(s:match "^(%d+)%.(%d+)%.(%d+)$")
+assert(n == x * 0x1000000 + y * 0x10000 + z * 0x100)
+assert("Mbed TLS "..s == f)
