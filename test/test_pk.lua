4206a83f8ab9c4229fdf2c86c09fb1429c5a5830
dev <dev@brigid.jp>
2024-04-06 13:23:19 +0900

    mbedtls 3.6.0に追随。

--- test/test_pk.lua
+++ test/test_pk.lua
@@ -9,5 +9,12 @@ assert(pk:write_key_pem() == test.secp256r1_1.key_pem)
 assert(pk:write_pubkey_pem() == test.secp256r1_1.pub_pem)
 
 local pk = mbedtls.pk():parse_public_key(test.secp256r1_1.pub_pem, ctr_drbg)
-assert(pk:write_key_pem() ~= test.secp256r1_1.key_pem)
+-- 秘密鍵が存在しない場合の挙動が変わった。
+-- v3.5.2: 空の秘密鍵
+-- v3.6.0: エラー
+if mbedtls.version.get_number() < 0x03060000 then
+  assert(pk:write_key_pem() ~= test.secp256r1_1.key_pem)
+else
+  test.assume_error(function () pk:write_key_pem() end)
+end
 assert(pk:write_pubkey_pem() == test.secp256r1_1.pub_pem)
